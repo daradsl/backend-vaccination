077e60be344a79d2eb135ae1e3646f89ceb66e72
daradsl <dara_dsl@outlook.com>
2022-04-22 14:37:45 -0300

    add add validation

--- src/controllers/AppointmentController.js
+++ src/controllers/AppointmentController.js
@@ -1,7 +1,33 @@
 import appointments from "../models/AppointmentModel.js";
+import appointmentAvailable from "../services/UsageRules.js";
+import crypto from "crypto";
+import { format} from "date-fns";
+import { appointmentSchema } from "../validators/SchemaValidator.js";
+
 class AppointmentController {
 	store(request, response) {
-		return response.send({ message: "Stored" });
+		const appointmentValidation = appointmentSchema.validate( request.body, { abortEarly: false});
+		if (appointmentValidation.error) {
+			return response.status(400).json({ error: appointmentValidation.error.details.map( ({ message }) => message ), message: "Invalid data",});
+		}
+		const { date, hour, name, birthDate } = request.body;
+
+		const appointment = {
+			id: crypto.randomUUID(),
+			accomplished: false,
+			status: "",
+			date: format(new Date(date), "yyyy/MM/dd"),
+			birthDate: format(new Date(birthDate), "yyyy/MM/dd"),
+			hour,
+			name,
+		};
+
+		const availableDate = appointmentAvailable(appointment.date, appointment.hour, appointments);
+		if(availableDate === "available"){
+			appointments.push(appointment);
+			return response.status(200).json({ message: "Appointment created" });
+		}
+		return response.status(403).json(availableDate);
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import express from "express";
+import router from "./routes/AppointmentRouter.js";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import cors from "cors";
-import router from "./routes/AppointmentRouter.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+
 app.use(cors());
-app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.json());
 app.use(router);
 
 app.listen(PORT, () => {
